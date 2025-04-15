872c9e7ed8e415316d1dc055fb3ff06de1c3cdac
Niyaz Khasanov <khasanchik85@gmail.com>
2025-04-15 19:48:58 +0500

    Созданы модели для модулей production_plan и shift_assignment Проведены миграции

--- production_plan/models.py
+++ production_plan/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class ProductionPlan(models.Model):
+    customer = models.CharField(max_length=255)
+    order = models.CharField(max_length=255)
+    product = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    plan = models.FileField(upload_to='production_plans/')
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='created_plans')
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='updated_plans', null=True, blank=True)
+    progress = models.IntegerField(default=0)  # Процент выполнения
+    deadline = models.DateTimeField()  # Срок отгрузки
+
+    def __str__(self):
+        return f'Заказ {self.order} для {self.customer}'
+
+    class Meta:
+        verbose_name = 'Production Plan'
+        verbose_name_plural = 'Production Plans'
--- shift_assignment/models.py
+++ shift_assignment/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class ShiftAssignment(models.Model):
+    customer = models.CharField(max_length=255)
+    date = models.DateField()
+    machine_number = models.IntegerField()
+    operator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='shifts')
+    order = models.CharField(max_length=255)
+    part = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    part_blueprint = models.FileField(upload_to='part_blueprints/')
+    execution_status = models.BooleanField(default=False)
+    comment = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'Сменное задание для {self.operator.username}'
+
+    class Meta:
+        verbose_name = 'Shift Assignment'
+        verbose_name_plural = 'Shift Assignments'
