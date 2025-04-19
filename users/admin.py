bd648d365a70e9c0e7db61b211237a6d58a2d723
Niyaz Khasanov <khasanchik85@gmail.com>
2025-04-19 13:59:45 +0500

    Добавлен административный интерфейс для модели User
    - Зарегистрирован класс UserAdmin с настройками отображения полей
    - Определены list_display, list_filter, search_fields для удобного управления пользователями
    - Добавлена сортировка по фамилии и имени в админке

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """
+    Административный интерфейс для модели User.
+
+    Этот класс определяет, какие поля будут отображаться в списке
+    объектов модели User в административной панели, а также
+    предоставляет возможность поиска и фильтрации.
+    """
+
+    # Поля, которые будут отображаться в списке объектов
+    list_display = ('username', 'last_name', 'first_name', 'role', 'pk', 'is_active')
+
+    # Поля, по которым можно фильтровать список объектов
+    list_filter = ('last_name', 'is_active')
+
+    # Поля, по которым можно осуществлять поиск
+    search_fields = ('username', 'last_name', 'first_name')
+
+    # Сортировка списка объектов по фамилии и имени
+    ordering = ('last_name', 'first_name')
