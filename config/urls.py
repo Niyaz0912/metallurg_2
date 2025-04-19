db3a1f19c2bbd55b7d7f2e30bd9f0138b536991a
Niyaz Khasanov <khasanchik85@gmail.com>
2025-04-19 23:21:22 +0500

    - Добавлено условие для обслуживания медиа-файлов через dev-сервер - Используется static() для маршрутизации MEDIA_URL к MEDIA_ROOT - Работает только при DEBUG=True для безопасности"

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 from users.views import CustomLoginView, CustomLogoutView, RegisterView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,3 +21,6 @@ urlpatterns = [
     path('production_plan/', include('production_plan.urls', namespace='production_plan')),
     path('shifts/', include('shift_assignment.urls', namespace='shift_assignment')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
