1e9e56690f33f3213675eae1e857278ae487fca5
Niyaz Khasanov <khasanchik85@gmail.com>
2025-04-19 23:19:47 +0500

    - Добавлен MEDIA_URL='/media/' - базовый URL для доступа к медиафайлам через веб-сервер - Определена MEDIA_ROOT - физический путь к папке для хранения загружаемых файлов   (os.path.join(BASE_DIR, 'media')) - Эти настройки необходимы для корректной работы с:   * Планами производства (ProductionPlan.plan)   * Чертежами деталей (ShiftAssignment.part_blueprint)   * Другими пользовательскими загрузками"

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
